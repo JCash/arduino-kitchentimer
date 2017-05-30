e8697fd5a7e95fa0a1224aa7d4fe5b218dbc2a7f
JCash <mwesterdahl76@gmail.com>
2017-05-31 01:04:51 +0200

    More updates Added AVR_DEBUG

--- build.sh
+++ build.sh
@@ -1,27 +1,32 @@
-
 ARDUINO_DIR=/Users/mawe/work/arduino/Arduino.app/Contents/Java
+ARDUINO_TOOLS_DIR=$ARDUINO_DIR/hardware/tools/avr/bin
+#ARDUINO_TOOLS_DIR=/usr/local/bin/
 
-GCC=$ARDUINO_DIR/hardware/tools/avr/bin/avr-gcc
-CXX=$ARDUINO_DIR/hardware/tools/avr/bin/avr-g++
-AR=$ARDUINO_DIR/hardware/tools/avr/bin/avr-gcc-ar
-NM=$ARDUINO_DIR/hardware/tools/avr/bin/avr-gcc-nm
-RANLIB=$ARDUINO_DIR/hardware/tools/avr/bin/avr-gcc-ranlib
-OBJCOPY=$ARDUINO_DIR/hardware/tools/avr/bin/avr-objcopy
-AVRDUDE=$ARDUINO_DIR/hardware/tools/avr/bin/avrdude
-AVRSIZE=$ARDUINO_DIR/hardware/tools/avr/bin/avr-size
+AVR_DEBUG=~/work/external/avr_debug
+
+GCC=$ARDUINO_TOOLS_DIR/avr-gcc
+CXX=$ARDUINO_TOOLS_DIR/avr-g++
+AR=$ARDUINO_TOOLS_DIR/avr-gcc-ar
+NM=$ARDUINO_TOOLS_DIR/avr-gcc-nm
+RANLIB=$ARDUINO_TOOLS_DIR/avr-gcc-ranlib
+OBJCOPY=$ARDUINO_TOOLS_DIR/avr-objcopy
+AVRDUDE=$ARDUINO_TOOLS_DIR/avrdude
+AVRSIZE=$ARDUINO_TOOLS_DIR/avr-size
 
 MCU=atmega328p
 PORT=/dev/cu.wchusbserial1410
 
-INCLUDES="-I $ARDUINO_DIR/hardware/arduino/avr/cores/arduino/ -I $ARDUINO_DIR/hardware/arduino/avr/variants/standard"
-CPPFLAGS="$INCLUDES -MMD -c -mmcu=$MCU -Wall -g -Os -ffunction-sections -fdata-sections -DF_CPU=16000000L -D__PROG_TYPES_COMPAT__ -fdiagnostics-color"
-CFLAGS="$CPPFLAGS -std=gnu11 -flto -fno-fat-lto-objects"
-CXXFLAGS="$CPPFLAGS -fno-threadsafe-statics -flto -fpermissive -fno-exceptions"
-ASFLAGS="-x assembler-with-cpp -flto"
-LDFLAGS="-w -Os -g -flto -fuse-linker-plugin -Wl,--gc-sections -mmcu=$MCU"
-
 BUILD=./build
 TARGET=kitchentimer
+
+INCLUDES="-I$ARDUINO_DIR/hardware/arduino/avr/cores/arduino/ -I$ARDUINO_DIR/hardware/arduino/avr/variants/standard"
+CPPOPT="-Os"
+CPPFLAGS="-MMD -c -mmcu=$MCU -Wall -g -gdwarf-2 -ffunction-sections -fdata-sections -DF_CPU=16000000L -D__PROG_TYPES_COMPAT__ -fdiagnostics-color"
+CFLAGS="-std=gnu11 -flto -fno-fat-lto-objects"
+CXXFLAGS="$CPPFLAGS -fno-threadsafe-statics -flto -fpermissive -fno-exceptions"
+ASFLAGS="-x assembler-with-cpp -flto"
+LDFLAGS="-w -Os -g -gdwarf-2 -flto -fuse-linker-plugin -Wl,--gc-sections -mmcu=$MCU -L$BUILD"
+LIBS="-lcore"
 
 set -e
 
@@ -29,7 +34,10 @@ mkdir -p $BUILD/
 
 
 #hack for testing
-#rm $BUILD/libcore.a
+# if [ -e "$BUILD/libcore.a" ]; then
+#     rm $BUILD/libcore.a
+#     rm $BUILD/libavrdebug.a
+# fi
 
 function CompileLibrary
 {
@@ -43,23 +51,40 @@ function CompileLibrary
 
         for f in $sourcedir/*.c
         do
+            [ -f "$f" ] || continue
+            if [ ! -z "$AVR_DEBUG" ]; then
+                if [ "$(basename $f)" == "WInterrupts.c" ]; then
+                    f=$AVR_DEBUG/arduino/1.8.1/WInterrupts.c
+                fi
+            fi
             out=$BUILD/${libbasename}_$(basename $f).o
-            echo "Compiling - $(basename $f) -> $out"
-            $GCC $CFLAGS $f -o $out
+            ##echo "Compiling - $(basename $f) -> $out"
+            echo "Compiling - $f -> $out"
+            $GCC $INCLUDES $CPPOPT $CPPFLAGS $CFLAGS $f -o $out
         done
 
         for f in $sourcedir/*.S
         do
+            [ -f "$f" ] || continue
             out=$BUILD/${libbasename}_$(basename $f).o
             echo "Compiling - $(basename $f) -> $out"
-            $CXX $CPPFLAGS $ASFLAGS $f -o $out
+            $CXX $INCLUDES $CPPOPT $CPPFLAGS $ASFLAGS $f -o $out
         done
 
         for f in $sourcedir/*.cpp
         do
+            [ -f "$f" ] || continue
+            if [ ! -z "$AVR_DEBUG" ]; then
+                if [ "$(basename $f)" == "HardwareSerial0.cpp" ]; then
+                    continue
+                fi
+                if [ "$(basename $f)" == "Print.cpp" ]; then
+                    continue
+                fi
+            fi
             out=$BUILD/${libbasename}_$(basename $f).o
             echo "Compiling - $(basename $f) -> $out"
-            $CXX $CXXFLAGS $f -o $out
+            $CXX $INCLUDES $CPPOPT $CPPFLAGS $CXXFLAGS $f -o $out
         done
 
         $AR rcs $libname $BUILD/${libbasename}_*.o
@@ -68,14 +93,25 @@ function CompileLibrary
     fi
 }
 
+if [ ! -z "$AVR_DEBUG" ]; then
+    echo "DEBUG ENABLED"
+    CPPOPT="-Og"
+    CPPFLAGS="-DAVR_DEBUG=1 $CPPFLAGS"
+    LIBS="-lavrdebug $LIBS"
+    INCLUDES="-I$AVR_DEBUG $INCLUDES"
+
+    echo "BUILDING" ./build/libcore.a
+    CompileLibrary $BUILD/libavrdebug.a $AVR_DEBUG/avr8-stub
+fi
+
 # Building the Arduino core manually
 CompileLibrary $BUILD/libcore.a $ARDUINO_DIR/hardware/arduino/avr/cores/arduino/
 
 echo "BUILDING SOURCE"
-$CXX $CXXFLAGS src/main.cpp -o $BUILD/main.o
+$CXX $INCLUDES $CPPOPT $CPPFLAGS $CXXFLAGS src/main.cpp -o $BUILD/main.o
 
 echo "LINKING $TARGET"
-$GCC $LDFLAGS -o $BUILD/$TARGET.elf $BUILD/main.o $BUILD/libcore.a -lm
+$GCC $LDFLAGS -o $BUILD/$TARGET.elf $BUILD/main.o $LIBS -lm
 
 echo "CREATING HEX"
 $OBJCOPY -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0  $BUILD/$TARGET.elf $BUILD/$TARGET.eep
@@ -85,4 +121,4 @@ echo "VERIFYING SIZE"
 $AVRSIZE --mcu=$MCU -C --format=avr $BUILD/$TARGET.elf
 
 echo "UPLOADING FLASH"
-$AVRDUDE -q -V -C$ARDUINO_DIR/hardware/tools/avr/etc/avrdude.conf -p$MCU -carduino -P$PORT -b115200 -D -Uflash:w:$BUILD/$TARGET.hex:i
+$AVRDUDE -V -C$ARDUINO_DIR/hardware/tools/avr/etc/avrdude.conf -p$MCU -carduino -P$PORT -b115200 -D -Uflash:w:$BUILD/$TARGET.hex:i
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,190 @@
 
 #include <Arduino.h>
 
-int frame = 0;
+#if defined(AVR_DEBUG)
+#include <avr8-stub/avr8-stub.h>
+#endif
+
+// Write to EEPROM: https://www.arduino.cc/en/Tutorial/EEPROMRead
+
+// CONSTANTS
+const int bnPinDown = 5;
+const int bnPinSelect = 6;
+const int bnPinStart = 7;
+const int bnPinUp = 8;
+// left to right
+const int bnDown    = 0;
+const int bnSelect  = 1;
+const int bnStart   = 2;
+const int bnUp      = 3;
+
+bool buttons[4] = {false,false,false,false};
+
+#define PRESSED HIGH
+
+int mode = 0;       // 0 - timer, 1 - game
+uint32_t prevtime = 0;  // The previous app time
+
+static void UpdateButtonStates()
+{
+    buttons[bnDown]     = digitalRead(bnPinDown) == PRESSED;
+    buttons[bnSelect]   = digitalRead(bnPinSelect) == PRESSED;
+    buttons[bnStart]    = digitalRead(bnPinStart) == PRESSED;
+    buttons[bnUp]       = digitalRead(bnPinUp) == PRESSED;
+}
+
+static void UpdateButtonStatesDebug(const String& command)
+{
+    if(command == "down")
+    {
+        buttons[bnDown] = true;
+    }
+    else if(command == "select")
+    {
+        buttons[bnSelect] = true;
+    }
+    else if(command == "start")
+    {
+        buttons[bnStart] = true;
+    }
+    else if(command == "up")
+    {
+        buttons[bnUp] = true;
+    }
+
+/*
+    Serial.print("buttons: ");
+    Serial.print(buttons[0]);
+    Serial.print(", ");
+    Serial.print(buttons[1]);
+    Serial.print(", ");
+    Serial.print(buttons[2]);
+    Serial.print(", ");
+    Serial.print(buttons[3]);
+    Serial.println("");
+    */
+}
+
+
+
+int timermode = 0; // 0 - set (not running), 1 - running, 2 - alarm
+int timersetmode = 0; // 0 - seconds, 1 - minutes
+uint32_t timertimeout = 0; // The timeout in milliseconds
+uint32_t timertime = 0;
+
+// Display time:
+// >= 60 minutes: 1:14h  where the ":" blinks on and off every second
+// < 60 minutes: 59:14 where the seconds are simply counting down
+
+static void TimerUpdate(uint32_t millis)
+{
+    if(timermode == 0)
+    {
+        if( buttons[bnSelect] )
+        {
+            timersetmode = (timersetmode + 1) & 1;
+
+            // Serial.print("Select mode");
+            // Serial.print(timersetmode);
+        }
+        else if( buttons[bnStart] )
+        {
+            if( timertimeout > 0 )
+            {
+                timermode = 1;
+                timertime = 0;
+
+//                Serial.print("Start timer!");
+            }
+        }
+        else if( buttons[bnUp] )
+        {
+            timertimeout += 1000;
+//            Serial.print(timertimeout / 1000);
+        }
+        else if( buttons[bnDown] )
+        {
+            if( timertimeout >= 1000)
+                timertimeout -= 1000;
+            
+//            Serial.print(timertimeout / 1000);
+        }
+
+        delay(500);
+    }
+    else if(timermode == 1)
+    {
+        timertime += millis;
+        if( timertime >= timertimeout )
+        {
+            timermode = 2;
+        }
+        else
+        {
+            delay(900);
+        }
+    }
+    else if(timermode == 2)
+    {
+        if( buttons[bnUp] || buttons[bnDown] || buttons[bnStart] || buttons[bnSelect] )
+        {
+            timermode = 0; // Back to timer set mode
+            timertime = 0;
+        }
+        else
+        {
+//            Serial.println("ALARM");
+            delay(1000);
+        }
+    }
+}
+
+
 void setup()
 {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(13, 0x1);
+#if defined(AVR_DEBUG)
+    debug_init();   // initialize the debugger
+#endif
 
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+    // Debugging
+    pinMode(13, OUTPUT);
+    //Serial.begin(9600);
+
+    // pins
+    // ARE they input as default? shave off 4 lines :)
+    pinMode(bnPinDown, INPUT);
+    pinMode(bnPinSelect, INPUT);
+    pinMode(bnPinStart, INPUT);
+    pinMode(bnPinUp, INPUT);
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(300);
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    delay(300);
+    // digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    // delay(300);
+    // digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    // delay(300);
 
-    Serial.println(frame);
-    ++frame;
+    String command;
+    // while (Serial.available()) {
+    //     delay(2);  //delay to allow byte to arrive in input buffer
+    //     char c = Serial.read();
+    //     command += c;
+    // }
+
+
+    uint32_t time = millis();
+    uint32_t deltamillis = time - prevtime;
+    if( time < prevtime )
+    {
+        deltamillis = 0xFFFFFFFF - prevtime + time; // Wrap around every 50 days :/
+    }
+    prevtime = time;
+
+    if( mode == 0 )
+    {
+        UpdateButtonStates();
+        UpdateButtonStatesDebug(command);
+        TimerUpdate(deltamillis);
+    }
 }
